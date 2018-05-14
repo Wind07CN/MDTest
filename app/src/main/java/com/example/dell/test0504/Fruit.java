3fa9820b1ee5a07ba959f4834f884afeaf9c3b0e
Wind7 <sunchen5575@126.com>
2018-05-14 17:06:50 +0800

    0514第二次提交

--- app/src/main/java/com/example/dell/test0504/Fruit.java
+++ app/src/main/java/com/example/dell/test0504/Fruit.java
@@ -1,4 +1,22 @@
 package com.example.dell.test0504;
 
 public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.imageId = imageId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
 }
--- app/src/main/java/com/example/dell/test0504/MainActivity.java
+++ app/src/main/java/com/example/dell/test0504/MainActivity.java
@@ -1,13 +1,164 @@
 package com.example.dell.test0504;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    private SwipeRefreshLayout swipeRefresh;
+
+    private Fruit[] fruits = {
+            new Fruit("Apple", R.drawable.f001), new Fruit("Banana", R.drawable.f002),
+            new Fruit("Grape", R.drawable.f003), new Fruit("Mango", R.drawable.f004)};
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.menu);
+        }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.warning:
+                        Toast.makeText(MainActivity.this, "Warning", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.candy:
+                        Toast.makeText(MainActivity.this, "Candy", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.color:
+                        Toast.makeText(MainActivity.this, "Color", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.layer:
+                        Toast.makeText(MainActivity.this, "Layer", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                }
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Data delete", Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Data restored", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
+
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+    }
+
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void initFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);  //设定随机的范围
+            fruitList.add(fruits[index]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.star:
+                Toast.makeText(this, "Star", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.location:
+                Toast.makeText(this, "Location", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.back:
+                Toast.makeText(this, "Back", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
