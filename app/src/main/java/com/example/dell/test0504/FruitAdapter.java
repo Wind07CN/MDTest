415067317a205bb613488d44d46d1ee45aaf1b04
Wind7 <sunchen5575@126.com>
2018-05-15 17:16:21 +0800

    0515更新

--- app/src/main/java/com/example/dell/test0504/FruitAdapter.java
+++ app/src/main/java/com/example/dell/test0504/FruitAdapter.java
@@ -1,11 +1,13 @@
 package com.example.dell.test0504;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,12 +24,14 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         CardView cardView;
         ImageView fruitImage;
         TextView fruitName;
+        ImageButton fruitButton;
 
         public ViewHolder(View view) {
             super(view);
             cardView = (CardView) view;
             fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
             fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            fruitButton = (ImageButton) view.findViewById(R.id.fruit_button);
         }
     }
 
@@ -41,7 +45,20 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
             mContext = parent.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+
+        return holder;
     }
 
     @Override
